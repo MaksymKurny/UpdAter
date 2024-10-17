d768460502b74a8786ab5b2597bb888c2100fc83
MaksymKurny <4680387@stud.nau.edu.ua>
2024-10-17 23:29:34 +0300

    fix

--- GDownloader.cs
+++ GDownloader.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SharpCompress.Archives;
-using SharpCompress.Archives.Rar;
-using SharpCompress.Archives.SevenZip;
-using SharpCompress.Archives.Zip;
 using SharpCompress.Common;
 using UpdAter.BL;
 
